7963adb2b809fbc1870e90cdb813d928ec33fa0b
Bleck <OverlordBleck@gmail.com>
2019-05-11 17:26:24 -0500

    Fixing some whoopsies

--- lua/autorun/server/sv_daily_restart.lua
+++ lua/autorun/server/sv_daily_restart.lua
@@ -26,14 +26,14 @@ end
 
 local function CFCDailyRestart:alertClientsOfServerRestart()
     print("Alerting clients of Restart")
-    restartAt = os.time() + (warningTime * 60)
+    CFCDailyRestart.restartAt = os.time() + (warningTime * 60)
 
     -- TODO: Alert how many seconds left
     -- You'll want a logarithmic-ish function to alert at:
     -- 15 min, 10min, 5min, 4min, 3min,2min,1min,30s,15s,10,9,8.. so on
 
 
-    sendAlertToClients()
+    CFCDailyRestart:sendAlertToClients()
 
     net.Start("AlertUsersOfRestart")
         net.WriteFloat(restartAt)
@@ -48,9 +48,8 @@ timer.Create("CFC_DailyRestart", 1, 0, function()
     if not lastScheduledRestart == nil then
         if os.difftime(os.time(), lastScheduledRestart) <= 3600 then return end
     end
-    -- if waitingForServerToEmpty then return
 
-    theTime = os.date("*t", os.time())
+    local theTime = os.date("*t", os.time())
 
     -- TODO: Also check each second within 1 hour leading up to the restart time. If the server is empty during this time, issue the restart and ensure we don't try to restart again at the desired time
     --
@@ -61,7 +60,7 @@ timer.Create("CFC_DailyRestart", 1, 0, function()
             local plyCount = table.Count(player.GetHumans())
 
             if plyCount == 0 then
-                restartServer()
+                CFCDailyRestart:restartServer()
             end
 
             -- When the server is not empty..
@@ -70,8 +69,8 @@ timer.Create("CFC_DailyRestart", 1, 0, function()
             -- You'll probably want to make a new function called waitForServerToEmpty() or something here and make sure the timer isn't triggering in the meantime
             -- You may want to keep a variable called "waitingForServerToEmpty" for this function to look at and no-op if so
             -- When the server is still not empty, you'll need to start the shutdown process. "countingDownToRestart" variable will tell this function not to re-initiate the process
-            countingDownToRestart = true
-            alertClientsOfServerRestart()
+            CFCDailyRestart:alertClientsOfServerRestart()
+            CFCDailyRestart.countingDownToRestart = true
         end
 
         return
