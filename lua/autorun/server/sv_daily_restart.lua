e90d1e0c6b9158c12a5093913a0a94992c4966f8
Bleck <OverlordBleck@gmail.com>
2019-05-11 17:22:13 -0500

    Localizing functions, comparing last restart

--- lua/autorun/server/sv_daily_restart.lua
+++ lua/autorun/server/sv_daily_restart.lua
@@ -1,10 +1,14 @@
 util.AddNetworkString("AlertUsersOfRestart")
-local desiredRestartTime = 3            -- The hour to initiate a restart. Must be between 0-24
-local warningTime = 15                  -- How many minutes players have until restart...
-local restartAt                         -- Time at which the server will restart
-local fileName = "CFC_LastDailyRestart"
 
-function restartServer()
+local CFCDailyRestart = {}
+CFCDailyRestart.desiredRestartTime      = 3                         -- The hour to initiate a restart. Must be between 0-24
+CFCDailyRestart.warningTime             = 15                        -- How many minutes players have until restart...
+CFCDailyRestart.restartAt               = nil                       -- Time at which the server will restart
+CFCDailyRestart.fileName                = "CFC_LastDailyRestart"
+CFCDailyRestart.countingDownToRestart   = false
+CFCDailyRestart.lastRestart             = file.Read(fileName..".txt", "DATA")
+
+local function CFCDailyRestart:restartServer()
     print("restarting server")
     
     file.Write(fileName..".txt", os.time())
@@ -14,19 +18,21 @@ function restartServer()
     --http.Post("localhost:2327/gmod/restart", params, function() end, function() end, headers)
 end
 
-function sendAlertToClients(message)
+local function CFCDailyRestart:sendAlertToClients(message)
     for k, v in pairs(player.GetHumans()) do
         v:ChatPrint(message)
     end
 end
 
-function alertClientsOfServerRestart()
+local function CFCDailyRestart:alertClientsOfServerRestart()
     print("Alerting clients of Restart")
     restartAt = os.time() + (warningTime * 60)
 
     -- TODO: Alert how many seconds left
     -- You'll want a logarithmic-ish function to alert at:
     -- 15 min, 10min, 5min, 4min, 3min,2min,1min,30s,15s,10,9,8.. so on
+
+
     sendAlertToClients()
 
     net.Start("AlertUsersOfRestart")
@@ -37,8 +43,11 @@ end
 --TODO: Create a timer to 
 
 timer.Create("CFC_DailyRestart", 1, 0, function()
-    -- TODO: Check to make sure that the latest restart time wasn't recently
-    -- if countingDownToRestart then return
+    local lastScheduledRestart = CFCDailyRestart.lastRestart
+
+    if not lastScheduledRestart == nil then
+        if os.difftime(os.time(), lastScheduledRestart) <= 3600 then return end
+    end
     -- if waitingForServerToEmpty then return
 
     theTime = os.date("*t", os.time())
@@ -47,22 +56,24 @@ timer.Create("CFC_DailyRestart", 1, 0, function()
     --
     -- TODO (Future): Perhaps if there are fewer than N players on the server leading up to the restart time, allow them to vote to restart now and get it out of the way
 
-    if (theTime.hour == desiredRestartTime) and (theTime.sec <= 30) then
-        local plyCount = table.Count(player.GetHumans())
+    if not countingDownToRestart then
+        if (theTime.hour == desiredRestartTime) and (theTime.sec <= 30) then
+            local plyCount = table.Count(player.GetHumans())
 
-        if plyCount == 0 then
-            timer.Remove("CFC_DailyRestart")
-            restartServer()
+            if plyCount == 0 then
+                restartServer()
+            end
+
+            -- When the server is not empty..
+            -- TODO: Wait up to 45 minutes for players to leave organically
+            -- If server still isn't empty by then, countdown restart
+            -- You'll probably want to make a new function called waitForServerToEmpty() or something here and make sure the timer isn't triggering in the meantime
+            -- You may want to keep a variable called "waitingForServerToEmpty" for this function to look at and no-op if so
+            -- When the server is still not empty, you'll need to start the shutdown process. "countingDownToRestart" variable will tell this function not to re-initiate the process
+            countingDownToRestart = true
+            alertClientsOfServerRestart()
         end
 
-        timer.Remove("CFC_DailyRestart")
-        -- When the server is not empty..
-        -- TODO: Wait up to 45 minutes for players to leave organically
-        -- If server still isn't empty by then, countdown restart
-        -- You'll probably want to make a new function called waitForServerToEmpty() or something here and make sure the timer isn't triggering in the meantime
-        -- You may want to keep a variable called "waitingForServerToEmpty" for this function to look at and no-op if so
-        -- When the server is still not empty, you'll need to start the shutdown process. "countingDownToRestart" variable will tell this function not to re-initiate the process
-        --countingDownToRestart = 1
-        alertClientsOfServerRestart()
+        return
     end
 end)
