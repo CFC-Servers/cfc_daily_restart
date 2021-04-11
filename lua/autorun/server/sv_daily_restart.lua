7408a4b26c28a621efad682b8bdcce3c46e13f98
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2021-04-11 17:56:51 -0600

    Fix syntax error

--- lua/autorun/server/sv_daily_restart.lua
+++ lua/autorun/server/sv_daily_restart.lua
@@ -455,7 +455,7 @@ DailyRestartTests.renew = function()
     test_waitUntilRestartHour()
 end
 
-hook.add( "PlayerSay", "CFC_DailyRestart_StopSoftRestart", function( ply, msg )
+hook.Add( "PlayerSay", "CFC_DailyRestart_StopSoftRestart", function( ply, msg )
     if msg ~= SOFT_RESTART_STOP_COMMAND then return end
     if not isValid( ply ) then return end
 
