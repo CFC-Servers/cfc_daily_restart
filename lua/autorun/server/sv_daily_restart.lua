6e4db0cdde1e4a7aece83b32129b1b2ba45bdedf
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2021-07-17 15:26:00 -0600

    Fix restart buffer

--- lua/autorun/server/sv_daily_restart.lua
+++ lua/autorun/server/sv_daily_restart.lua
@@ -422,7 +422,8 @@ local function waitForNextSoftRestartWindow()
 
     local timeUntilNextWindowAlert = window.timeSinceStart * SECONDS_IN_HOUR - timeSinceStart() - LARGEST_ALERT_INTERVAL
 
-    if os.time() + timeUntilNextWindowAlert + RESTART_BUFFER < EARLIEST_RESTART_TIME then return end -- Hard restart is too close
+    if os.time() + timeUntilNextWindowAlert < EARLIEST_RESTART_TIME then return end -- Too early to restart
+    if timeUntilNextWindowAlert + RESTART_BUFFER > getHoursUntilRestartHour() * SECONDS_IN_HOUR then return end -- Hard restart is too close
 
     timer.Create( SoftRestartTimerName, timeUntilNextWindowAlert, 1, function()
         if #player.GetHumans() <= window.playerMax then
