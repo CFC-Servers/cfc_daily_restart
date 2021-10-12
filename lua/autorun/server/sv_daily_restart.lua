4534a457d626c11b85d3786a673a0a485165889d
legokidlogan <legokidlogan@gmail.com>
2021-10-12 14:04:57 -0600

    Check player validity

--- lua/autorun/server/sv_daily_restart.lua
+++ lua/autorun/server/sv_daily_restart.lua
@@ -547,7 +547,7 @@ function CFCDailyRestart.stopSoftRestart( ply, hidePrint )
     local mixPanelData = { playerCount = player.GetCount(), amountOfStops = stopCount }
     CFCDailyRestart.numSoftStops = stopCount
 
-    if ply then
+    if IsValid( ply ) and ply:GetClass() == "player" then
         mixpanelTrackPlyEvent( "Ply stopped soft restart", ply, mixPanelData )
     else
         mixpanelTrackEvent( "Soft restart stopped", mixPanelData )
